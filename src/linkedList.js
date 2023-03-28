e3af0019d70617b4b445287225f52b80a9e89f57
Yasen Dimitrov <y_dimitrov@ymail.com>
2023-03-28 09:38:34 +0300

    Add Node class to LL project

--- src/linkedList.js
+++ src/linkedList.js
@@ -1,3 +1,28 @@
-class LinkedList { }
+class LinkedList {}
 
-class Node { }
+class Node {
+  constructor(value = null, nextNode = null) {
+    this._value = value;
+    this._nextNode = nextNode;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(newValue) {
+    this._value = newValue;
+  }
+
+  get nextNode() {
+    return this._nextNode;
+  }
+
+  set nextNode(newNode) {
+    this._nextNode = newNode;
+  }
+}
+
+const nodeOne = new Node();
+
+console.log(nodeOne.value);
